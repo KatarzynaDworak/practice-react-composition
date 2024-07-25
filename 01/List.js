1d54e6d6ff1f3217b281847c61bf5b1362613406
Katarzyna Dworak <k.dw@poczta.onet.pl>
2024-07-25 14:55:39 +0200

    task 01 done

--- 01/List.js
+++ 01/List.js
@@ -1,13 +1,29 @@
 import React from "react";
 
 const List = props => {
-        const { items } = props;
-        const list = items.map(n => <Item key={n} name={n} />)
+    const { items } = props;
 
-        return <ul>{ list }</ul>
+    const listStyle = {
+        listStyleType: 'none',
+        padding: 0,
+        margin: 0,
+    };
+
+    const listItemStyle = {
+        padding: '10px 20px',
+        margin: '5px 0',
+        backgroundColor: '#f2f2f2',
+        border: '1px solid #ddd',
+        borderRadius: '4px',
+    };
+
+    const list = items.map(n => <Item key={n} name={n} style={listItemStyle} />)
+
+    return <ul style={listStyle}>{ list }</ul>
 }
 
 const Item = props => {
-    return <li key={props.name}>{ props.name }</li>
+    return <li style={props.style}>{ props.name }</li>
 }
+
 export default List;
